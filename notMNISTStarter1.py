ad355a84db323d70516947588d5a1008683c324c
Cristhian Sotelo <37880528+ChrisBreak@users.noreply.github.com>
2019-12-06 22:12:58 -0700

    improved CHDModel, has regularizer and dropout

--- notMNISTStarter1.py
+++ notMNISTStarter1.py
@@ -19,13 +19,20 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 
 print("--Make model--")
 model = tf.keras.Sequential([
-    tf.keras.layers.Conv1D(32, 3, activation='relu',
-                           kernel_regularizer=tf.keras.regularizers.l2(0.005),
-                           input_shape=(28, 28)),
-    tf.keras.layers.MaxPooling1D(2),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(512, activation='relu'),
-    tf.keras.layers.Dense(10, activation='softmax')
+  preprocessing_layer,
+  tf.keras.layers.Dense(512, kernel_regularizer=tf.keras.regularizers.l2(0.005),
+                 activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(512, kernel_regularizer=tf.keras.regularizers.l2(0.005),
+                 activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(512, kernel_regularizer=tf.keras.regularizers.l2(0.005),
+                 activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(512, kernel_regularizer=tf.keras.regularizers.l2(0.005),
+                 activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
